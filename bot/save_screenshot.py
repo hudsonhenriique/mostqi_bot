5ffd826891146fab9797b9d61c2c5ddb54f9c93b
Hudson <hudson.henri10@gmail.com>
2025-05-12 16:29:22 -0300

    Removed unused save_screenshot.py file

--- bot/save_screenshot.py
+++
@@ -1,5 +0,0 @@
-async def save_screenshot(page, filename):
-    # Save a screenshot of the page
-    await page.screenshot(path=filename)
-    print(f"Screenshot saved as {filename}")
-    return filename
