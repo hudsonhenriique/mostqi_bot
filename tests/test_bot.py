305521062d0289d3de5df96f76fac8dde7e2d571
Hudson <hudson.henri10@gmail.com>
2025-05-14 14:39:01 -0300

    test: add verification for social filter in tests
    - Updated the 	est_run_bot_with_cpf to include the social_filter=True parameter.
    - Added an assert to verify if the social filter is correctly marked in the result.
    - Ensured the test validates the expected behavior when using the social filter.

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -4,9 +4,9 @@ from models.outputs import SearchOutput
 
 @pytest.mark.asyncio
 async def test_run_bot_with_cpf():
-    cpf = "12345678900"  # Use um valor de teste
+    cpf = "12345678900"  # vauelue for testing
 
-    result = await run_bot(cpf=cpf)
+    result = await run_bot(cpf=cpf,social_filter=True)
 
     assert isinstance(result, SearchOutput)
     assert result.status in ("success", "error")
@@ -15,4 +15,4 @@ async def test_run_bot_with_cpf():
         assert result.file is not None
         assert result.image_base64 is not None
         assert "cpf" in result.query
-
+        assert result.query["filtro_social"] is True
